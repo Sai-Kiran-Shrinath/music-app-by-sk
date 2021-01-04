6173f7fbe68327989be0b06ae428b88ad7900bd3
Sai Kiran Shrinath <saikiranshrinath912@gmail.com>
2021-01-04 21:57:04 +0530

    done2

--- src/App.js
+++ src/App.js
@@ -11,50 +11,55 @@ import {
   NavbarBrand,
   Nav,
   NavItem,
-  NavLink,
 } from "reactstrap";
+import { NavLink } from "react-router-dom";
 
 function App() {
   const [collapsed, setCollapsed] = useState(true);
   const toggleNav = () => setCollapsed(!collapsed);
   return (
     <>
-      <Navbar className="navbar" dark expand="md">
-        <div className="container">
-          <NavbarBrand href="/home" className="mr-auto">
-            <img src="/favicon.jpg" height="40px" width="40px" alt="logo"></img>
-            <span className="navbar-brand">SK's Music App</span>
-          </NavbarBrand>
-          <NavbarToggler onClick={toggleNav} className="mr-2" />
-          <Collapse isOpen={!collapsed} navbar>
-            <Nav navbar>
-              <NavItem>
-                <NavLink href="/home" className="navlink">
-                  <span className="fa fa-home nav-text">
-                    <span className="fontstyle"> Home</span>
-                  </span>
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/playlist">
-                  <span className="fa fa-music nav-text">
-                    <span className="fontstyle"> Playlist</span>
-                  </span>
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/artists">
-                  <span className="fa fa-info nav-text">
-                    <span className="fontstyle"> Artists</span>
-                  </span>
-                </NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </div>
-      </Navbar>
-
       <BrowserRouter>
+        <Navbar className="navbar" dark expand="md">
+          <div className="container">
+            <NavbarBrand href="/home" className="mr-auto">
+              <img
+                src="/favicon.jpg"
+                height="40px"
+                width="40px"
+                alt="logo"
+              ></img>
+              <span className="navbar-brand">SK's Music App</span>
+            </NavbarBrand>
+            <NavbarToggler onClick={toggleNav} className="mr-2" />
+            <Collapse isOpen={!collapsed} navbar>
+              <Nav navbar>
+                <NavItem>
+                  <NavLink to="/home" className="navlink">
+                    <span className="fa fa-home nav-text">
+                      <span className="fontstyle"> Home</span>
+                    </span>
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink to="/playlist">
+                    <span className="fa fa-music nav-text">
+                      <span className="fontstyle"> Playlist</span>
+                    </span>
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink to="/artists">
+                    <span className="fa fa-info nav-text">
+                      <span className="fontstyle"> Artists</span>
+                    </span>
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </div>
+        </Navbar>
+
         <Switch>
           <Route path="/home" component={Home} />
           <Route path="/playlist" component={Playlist} />
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Songs } from "../songsinfo";
+import { Link } from "react-router-dom";
 
 function Home() {
   const songCard = Songs.map((song) => {
@@ -9,14 +10,14 @@ function Home() {
           className="row text-center"
           style={{ borderBottom: "3px solid #070047", paddingBottom: "15px" }}
         >
-          <a href="/playlist" className="col-12 col-md-4">
+          <Link to="/playlist" className="col-12 col-md-4">
             <img src={song.image} alt={song.name} height="200px" width="100%" />
-          </a>
+          </Link>
           <div className="col-12 col-md-8">
             <h5>
-              <a style={{ textDecoration: "none" }} href="/playlist">
+              <Link to="/playlist" href="/playlist">
                 <strong style={{ color: "rgb(92, 1, 8)" }}>{song.name}</strong>
-              </a>
+              </Link>
             </h5>
             <p>By {song.artist}</p>
             <p>Released in {song.released}</p>
